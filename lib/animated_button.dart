07db6c7b600edc9c0c3a39428262f1b96f740cba
vorrarit <vorrarit@gmail.com>
2019-05-19 09:27:39 +0700

    initial commit

---
+++ lib/animated_button.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class AnimatedButton extends StatefulWidget {
+  var text = "";
+  var icon = Icons.add;
+  Color color = Colors.pinkAccent;
+  var duration = Duration(seconds: 1);
+
+  Function onTap;
+
+  AnimatedButton({this.text, this.icon, this.color, this.duration, this.onTap});
+
+  @override
+  _AnimatedButtonState createState() {
+    return _AnimatedButtonState();
+  }
+}
+
+class _AnimatedButtonState extends State<AnimatedButton> with SingleTickerProviderStateMixin {
+  var isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildAnimated();
+  }
+
+  Widget _buildAnimated() {
+    // return AnimatedContainer(
+    //   duration: Duration(seconds: 5),
+    //   child: buildNotAnimated(),
+    //   curve: Curves.bounceInOut,
+    // );
+
+    return _buildNotAnimated();
+  }
+
+  Row _buildNotAnimated() {
+    return Row(
+      children: <Widget>[
+        InkWell(
+          splashColor: Colors.transparent,
+          child: AnimatedContainer(
+            duration: widget.duration,
+            curve: Curves.easeInOut,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                color: isPressed ? widget.color.withOpacity(0.5): Colors.white
+            ),
+            padding: EdgeInsets.only(left: 16, top: 8, right: 16, bottom: 8),
+            child: Row(
+              children: [
+                Icon(widget.icon, color: (isPressed? widget.color: Colors.black)),
+                Padding(padding: EdgeInsets.only(right: 8)),
+                AnimatedSize(
+                  duration: widget.duration,
+                  curve: Curves.easeInOut,
+                  vsync: this,
+                  child: Text(
+                    isPressed ? widget.text: '',
+                    style: TextStyle(
+                      color: (isPressed? widget.color: Colors.black),
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18
+                    )
+                  ),
+                )
+              ]
+            ),
+          ),
+          onTap: onTap,
+        ),
+      ],
+    );
+  }
+
+  void onTap() {
+    setState(() {
+      isPressed = !isPressed;
+    });
+    widget.onTap();
+  }
+
+  void selected(bool isSelected) {
+    setState(() {
+      isPressed = isSelected;
+    });
+  }
+}
---
+++ lib/bottom_navigation_bar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'animated_button.dart';
+
+class BottomNavigationBar extends StatefulWidget {
+
+  var buttons = [
+    { 'text': 'Home', 'icon': Icons.home, 'color': Colors.amber.shade300 },
+    { 'text': 'Profiles', 'icon': Icons.portrait, 'color': Colors.red.shade300 },
+    { 'text': 'Contacts', 'icon': Icons.contacts, 'color': Colors.green.shade300 },
+    { 'text': 'Store', 'icon': Icons.shopping_cart, 'color': Colors.blue.shade300 }
+  ];
+
+  BottomNavigationBar({
+    Key key,
+  }) : super(key: key);
+
+
+  @override
+  _BottomNavigationBarState createState() {
+    return _BottomNavigationBarState();
+  }
+}
+
+class _BottomNavigationBarState extends State<BottomNavigationBar> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 8,
+      child: Container(
+        padding: EdgeInsets.all(8),
+        child: Row(
+          children: _buildButtons(),
+        )
+      ),
+    );
+  }
+
+  List<Widget> _buildButtons() {
+    List<Widget> _buttonWidgets = [];
+    for (var i=0; i<widget.buttons.length; i++) {
+      _buttonWidgets.add(
+        AnimatedButton(
+          color: widget.buttons[i]['color'],
+          duration: Duration(milliseconds: 300),
+          icon: widget.buttons[i]['icon'],
+          text: widget.buttons[i]['text'],
+          onTap: () => onTap(i),
+        )
+      );
+    }
+    return _buttonWidgets;
+  }
+
+  onTap(int i) {
+    setState(() {
+      selectedIndex = i;
+    });
+  }
+}
